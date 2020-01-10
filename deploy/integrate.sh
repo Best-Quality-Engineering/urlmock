f0f31b22e360388bfabb0febd4aeafa71acf553b
Jeff Glatz <jeff@bestquality.engineering>
2020-01-10 07:03:31 -0600

    Update integrate.sh

--- deploy/integrate.sh
+++ deploy/integrate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -v
+# Deploy a snapshot release if not tagged
 if [ -z "${TRAVIS_TAG}" ]; then
     mvnw deploy -e -B --no-transfer-progress -settings .mvn/settings.xml -P ossrh
 fi
