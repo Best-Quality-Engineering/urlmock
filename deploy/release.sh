4075515f9ed4bed86c938525a68a34fb76b4bb26
Jeff Glatz <jeff@kungzhi.com>
2020-01-10 14:03:58 -0600

    Don't use maven to compute snapshot version

--- deploy/release.sh
+++ deploy/release.sh
@@ -9,10 +9,8 @@ git add pom.xml
 git commit --message "Release ${TRAVIS_TAG} (build: ${TRAVIS_BUILD_NUMBER})"
 # Set the next development snapshot version
 mvnw release:update-versions -e -B -ntp -P ossrh
-# Capture the snapshot version
-SNAPSHOT=$(mvnw org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version | egrep -v "(^\[INFO\])")
 # Commit the changes
 git add pom.xml
-git commit --message "Snapshot ${SNAPSHOT} (build: ${TRAVIS_BUILD_NUMBER})"
+git commit --message "Next development version (build: ${TRAVIS_BUILD_NUMBER})"
 # Push changes to origin
 git push -u ssh-origin master
--- deploy/snapshot.sh
+++ deploy/snapshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Deploy a snapshot release if not tagged
 if [ -z "${TRAVIS_TAG}" ]; then
-  echo "Integrating $(mvnw org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version | egrep -v "(^\[INFO\])")"
+  echo "Performing a snapshot build"
   mvnw deploy -e -B -ntp -settings .mvn/settings.xml -P ossrh
 fi
