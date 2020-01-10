9efbc89fd69e3e327b408a5dee88957c70c476cd
Jeff Glatz <jeff@bestquality.engineering>
2020-01-10 07:02:09 -0600

    Update integrate.sh

--- deploy/integrate.sh
+++ deploy/integrate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -v
-bundle exec rake:units
-if [ "${TRAVIS_TAG}" ]; then
-  
+if [ -z "${TRAVIS_TAG}" ]; then
+    mvnw deploy -e -B --no-transfer-progress -settings .mvn/settings.xml -P ossrh
 fi
