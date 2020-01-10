deab25c74b840ac05d40a50c7321a6e283ff218a
Jeff Glatz <jeff@bestquality.engineering>
2020-01-10 07:05:50 -0600

    Delete integrate.sh

--- deploy/integrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -v
-# Deploy a snapshot release if not tagged
-if [ -z "${TRAVIS_TAG}" ]; then
-    mvnw deploy -e -B --no-transfer-progress -settings .mvn/settings.xml -P ossrh
-fi
