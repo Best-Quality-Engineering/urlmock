a89f6888bfd70f474d2eb498e72e0250991d876a
Jeff Glatz <jeff@kungzhi.com>
2020-01-09 23:00:27 -0600

    Silence maven transfer progress

--- deploy/release.sh
+++ deploy/release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Set the version to build
-mvnw versions:set -e -B -DnewVersion=${TRAVIS_TAG} -DgenerateBackupPoms=false
+mvnw versions:set -e -B --no-transfer-progress -DnewVersion=${TRAVIS_TAG} -DgenerateBackupPoms=false
 # Build, install, test then publish the artifacts
-mvnw clean deploy -e -B -settings .mvn/settings.xml -P ossrh
+mvnw clean deploy -e -B --no-transfer-progress -settings .mvn/settings.xml -P ossrh
 # Set the next development snapshot version
-mvnw release:update-versions -e -B -P ossrh
+mvnw release:update-versions -e -B --no-transfer-progress -P ossrh
 # Commit the changes
 git checkout -b release/${TRAVIS_TAG}
 git add pom.xml
