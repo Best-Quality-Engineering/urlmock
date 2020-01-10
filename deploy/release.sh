7c61b7910ba56a61b842c0ee174bd9bba6a55235
Jeff Glatz <jeff@kungzhi.com>
2020-01-09 22:42:30 -0600

    add prerequisites to pom

--- deploy/release.sh
+++ deploy/release.sh
@@ -6,7 +6,8 @@ mvnw -e -B clean deploy -settings .mvn/settings.xml -P ossrh
 # Set the next development snapshot version
 mvnw -e -B release:update-versions -B
 # Commit the changes
+git checkout -b release/${TRAVIS_TAG}
 git add pom.xml
 git commit --message "Next development version (build: ${TRAVIS_BUILD_NUMBER})"
 git remote add origin git@github.com:ruffkat/urlmock.git >/dev/null 2>&1
-git push --set-upstream origin master --quiet
+git push --set-upstream origin release/${TRAVIS_TAG} --quiet
