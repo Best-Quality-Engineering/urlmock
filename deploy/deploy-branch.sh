4488464a58571f602250bb25eb000bec18a8e658
Jeff Glatz <jeff@kungzhi.com>
2020-01-11 17:57:10 -0600

    Fixed typo

--- deploy/deploy-branch.sh
+++ deploy/deploy-branch.sh
@@ -2,5 +2,5 @@
 # Only deploy branch builds
 if [ "${TRAVIS_BRANCH}" != "${TRAVIS_TAG}" ]; then
   echo "Performing a deploy on ${TRAVIS_BRANCH}"
-  mvnw mvnw -e -B -ntp -settings .mvn/settings.xml -P ossrh deploy
+  mvnw -e -B -ntp -settings .mvn/settings.xml -P ossrh deploy
 fi
