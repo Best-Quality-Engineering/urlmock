3d235c507a45bbc58808c9aaced0e24abc09c97d
Jeff Glatz <jeff@kungzhi.com>
2020-01-12 13:05:51 -0600

    Update release workflow

--- deploy/release-tag.sh
+++ deploy/release-tag.sh
@@ -2,9 +2,16 @@
 echo "Releasing ${TRAVIS_TAG}"
 
 # When triggered by a tag, the local git repository will be in
-# a detached head mode, so create the release branch for it.
+# a detached head mode, so configure it for pushing changes.
+echo "Configuring git to release"
+git config --local user.name "Travis CI"
+git config --local user.email "travis@travis-ci.com"
+git config --unset-all remote.origin.fetch
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch
+
 echo "Creating release branch: release/${TRAVIS_TAG}"
-git switch -c release/${TRAVIS_TAG}
+git checkout -b release/${TRAVIS_TAG}
 
 echo "Setting POM versions to ${TRAVIS_TAG}"
 mvn -e -B -ntp -s deploy/settings.xml -P ossrh -DgenerateBackupPoms=false -DnewVersion=${TRAVIS_TAG} versions:set
@@ -14,18 +21,20 @@ mvn -e -B -ntp -s deploy/settings.xml -P ossrh clean deploy
 
 echo "Committing changes to release/${TRAVIS_TAG}"
 git add pom.xml
-git commit --message "Release ${TRAVIS_TAG} (build: ${TRAVIS_BUILD_NUMBER})"
-git push -u ssh-origin release/${TRAVIS_TAG}
+git commit -m "Release ${TRAVIS_TAG} (build: ${TRAVIS_BUILD_NUMBER})"
+git push -u origin release/${TRAVIS_TAG}
 
 echo "Merging release/${TRAVIS_TAG} into master"
-git fetch
 git checkout master
 git pull
 git merge release/${TRAVIS_TAG}
-git push -u ssh-origin master
 
 echo "Preparing next development version"
 mvn -e -B -ntp -s deploy/settings.xml -P ossrh release:update-versions
+
+echo "Executing deploy goal next development version"
+mvn -e -B -ntp -s deploy/settings.xml -P ossrh clean deploy
+
 git add pom.xml
-git commit --message "Next development version (build: ${TRAVIS_BUILD_NUMBER})"
-git push -u ssh-origin master
+git commit -m "Next development version (build: ${TRAVIS_BUILD_NUMBER})"
+git push -u origin master
